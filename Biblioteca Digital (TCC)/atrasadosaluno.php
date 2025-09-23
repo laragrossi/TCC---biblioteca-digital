581b725740ff205791a418e7579601d965a34504
jamilly1912 <jamillybrito847@gmail.com>
2025-09-23 15:45:04 -0300

    Add files via upload

---
+++ Biblioteca Digital (TCC)/atrasadosaluno.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Empréstimos Ativos</title>
+
+<!-- Importando ícones do Bootstrap -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+<link rel="stylesheet" href="atrasadosalunos.css">
+
+</head>
+<body>
+
+<header>
+    <h1>Empréstimos ativos</h1>
+    <div class="icons">
+        <a href="homealuno.php"><i class="bi bi-house-door"></i></a>
+        <i class="bi bi-bell"></i>
+        <a href="dadosaluno.php"><i class="bi bi-person"></i></a>
+    </div>
+</header>
+
+<div class="search-container">
+    <div class="search-box">
+        <i class="bi bi-search"></i>
+        <input type="text" placeholder="Procurar alunos">
+    </div>
+</div>
+
+<div class="filter-buttons">
+    <a href="emprestimoaluno.php" class="filter-btn active">Todos</a>
+    <button class="filter-btn disabled">Atrasados</button>
+</div>
+
+<div class="cards">
+    <div class="card">
+        <h3>Nome do aluno</h3>
+        <p>RA: XXXXXXXXXX-X</p>
+        <p>Turma: 3ºDS</p>
+        <p>Livro: O Cortiço</p>
+        <span class="status em-atraso">Em atraso</span>
+        <p>Empréstimo: xx/xx/xxxx</p>
+        <p>Devolução: xx/xx/xxxx</p>
+    </div>
+
+    <div class="card">
+        <h3>Nome do aluno</h3>
+        <p>RA: XXXXXXXXXX-X</p>
+        <p>Turma: 3ºDS</p>
+        <p>Livro: O Pequeno Príncipe</p>
+        <span class="status em-atraso">Em atraso</span>
+        <p>Empréstimo: xx/xx/xxxx</p>
+        <p>Devolução: xx/xx/xxxx</p>
+    </div>
+
+    <div class="card">
+        <h3>Nome do aluno</h3>
+        <p>RA: XXXXXXXXXX-X</p>
+        <p>Turma: 3ºDS</p>
+        <p>Livro: Dom Casmurro</p>
+        <span class="status em-atraso">Em atraso</span>
+        <p>Empréstimo: xx/xx/xxxx</p>
+        <p>Devolução: xx/xx/xxxx</p>
+    </div>
+
+    <div class="card">
+        <h3>Nome do aluno</h3>
+        <p>RA: XXXXXXXXXX-X</p>
+        <p>Turma: 3ºDS</p>
+        <p>Livro: O Alienista</p>
+        <span class="status em-atraso">Em atraso</span>
+        <p>Empréstimo: xx/xx/xxxx</p>
+        <p>Devolução: xx/xx/xxxx</p>
+    </div>
+</div>
+
+
+</body>
+</html>
---
+++ Biblioteca Digital (TCC)/cadastroaluno.php
@@ -0,0 +1,87 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+       include_once('config.php');
+
+       $nome = $_POST['nome'];
+       $ra = $_POST['ra'];
+       $digito = $_POST['digito'];
+       $senha = $_POST['senha'];
+       $escola = $_POST['escola'];
+
+       $result = mysqli_query($conexao, "INSERT INTO tabela(nome,ra,digito,senha,escola,) 
+       VALUES ('$nome','$ra','$digito','$senha','$escola')");
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en"> <!-- Define o tipo de documento e a linguagem -->
+<head>
+    <meta charset="UTF-8"> <!-- Define a codificação de caracteres -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Deixa o site responsivo -->
+    <title>Cadastro</title> <!-- Título da aba do navegador -->
+    
+    <!-- Link para o nosso arquivo de CSS personalizado -->
+    <link rel="stylesheet" href="cadastroaluno.css">
+</head>
+<body>
+    <!-- Caixa central -->
+    <div class="box">
+        <!-- Formulário de cadastro -->
+        <form action="formulario.php" method="POST">
+            <fieldset>
+                <legend><b>Cadastro do Aluno</b></legend> <!-- Título do formulário -->
+                <br>
+
+                <!-- Campo Nome -->
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput">Nome Completo</label>
+                </div>
+                <br><br>
+
+                <!-- Campo RA -->
+                <div class="inputBox">
+                    <input type="text" name="ra" id="ra" class="inputUser" required>
+                    <label for="ra" class="labelInput">RA</label>
+                </div>
+                <br><br>
+
+                <!-- Campo Digito -->
+                <div class="inputBox">
+                    <input type="text" name="digito" id="digito" class="inputUser" required>
+                    <label for="digito" class="labelInput">Digito</label>
+                </div>
+                <br><br>
+
+                <!-- Campo Senha -->
+                <div class="inputBox">
+                    <input type="text" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput">Senha</label>
+                </div>
+                <br><br>
+
+                <!-- Campo csenha -->
+                <div class="inputBox">
+                    <input type="text" name="csenha" id="csenha" class="inputUser" required>
+                    <label for="csenha" class="labelInput">Confirmar Senha</label>
+                </div>
+                <br><br>
+
+                <!-- Campo Escola -->
+                <div class="inputBox">
+                    <input type="text" name="escola" id="escola" class="inputUser" required>
+                    <label for="escola" class="labelInput">Escola</label>
+                </div>
+                <br><br>
+
+                <!-- Botão de cadastro -->
+                <input type="submit" name="submit" id="submit" value="Cadastrar-se">
+                <br><br>
+                <input href="loginaluno.php" type="submit" name="submit" id="submit" value="Login">             
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>

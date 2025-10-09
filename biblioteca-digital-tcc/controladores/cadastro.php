a1049f0077ffa9a23c97fcce65f5d224599b3205
giovana1200 <giovanargreco@gmail.com>
2025-10-09 14:14:56 -0300

    Cadastro
    colocamos a frase de cadastro feito com sucesso no professor e aluno

--- biblioteca-digital-tcc/controladores/cadastro.php
+++ biblioteca-digital-tcc/controladores/cadastro.php
@@ -1,31 +1,35 @@
 <?php
+if (isset($_POST['submit'])) {
+    include_once('../config.php');
 
-    if(isset($_POST['submit']))
-    {
-        include_once('../config.php');
-       if (isset($_POST['ra'])) {
-
-       $nome = $_POST['nome'];
-       $ra = $_POST['ra'];
-       $digito = $_POST['digito'];
-       $senha = $_POST['senha'];
-       $escola = $_POST['escola'];
-
-       $result = mysqli_query($conexao, "INSERT INTO aluno(nome,ra,digito,senha,escola) 
-       VALUES ('$nome','$ra','$digito','$senha','$escola')");
-       header('Location: ../loginaluno.php');
-       } else {
+    if (isset($_POST['ra'])) {
+        // Cadastro do aluno
         $nome = $_POST['nome'];
-       $email = $_POST['email'];
-       $cidade = $_POST['cidade'];
-       $senha = $_POST['senha'];
-       $escola = $_POST['escola'];
+        $ra = $_POST['ra'];
+        $digito = $_POST['digito'];
+        $senha = $_POST['senha'];
+        $escola = $_POST['escola'];
 
-       $result = mysqli_query($conexao, "INSERT INTO professor(nome, email,senha,escola, cidade) 
-       VALUES ('$nome','$email','$senha','$escola','$cidade')");
-       header('Location: ../loginprof.php');
-       }
+        $result = mysqli_query($conexao, "INSERT INTO aluno(nome, ra, digito, senha, escola) 
+            VALUES ('$nome', '$ra', '$digito', '$senha', '$escola')");
+        
+        // Redireciona passando a mensagem pela URL
+        header('Location: ../loginaluno.php?mensagem=sucesso');
+        exit();
+    } else {
+        // Cadastro do professor
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $cidade = $_POST['cidade'];
+        $senha = $_POST['senha'];
+        $escola = $_POST['escola'];
+
+        $result = mysqli_query($conexao, "INSERT INTO professor(nome, email, senha, escola, cidade) 
+            VALUES ('$nome', '$email', '$senha', '$escola', '$cidade')");
+        
+        // Redireciona passando a mensagem pela URL
+        header('Location: ../loginprof.php?mensagem=sucesso');
+        exit();
     }
-
+}
 ?>
-
